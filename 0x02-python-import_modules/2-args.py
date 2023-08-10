f5c7a3ac9799a2a106941d1113cbf8277cfec6e0
Anyoh98 <fonachuanyoh@yahoo.com>
2023-08-10 16:29:03 -0700

    just testing the code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
     num_args = len(sys.argv)
     arg_index = len(sys.argv) - 1
     if arg_index == 1:
-        print("{} argument:".format(arg_index))
+        print("1 argument:")
     elif arg_index == 0:
-        print("{} arguments.".format(arg_index))
+        print("0 arguments.")
     else:
         print("{} arguments.".format(arg_index))
     for num in range(arg_index):
