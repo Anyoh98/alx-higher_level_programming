69c18d8a86f44bc1ee43c2cdef4e4dd389c9c0e9
Anyoh98 <fonachuanyoh@yahoo.com>
2023-08-10 16:17:42 -0700

    testing code

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
     from calculator_1 import add, sub, mul, div
-    result1 = add(a, b) 
+    result1 = add(a, b)
     result2 = sub(a, b)
     result3 = mul(a, b)
     result4 = div(a,b)
