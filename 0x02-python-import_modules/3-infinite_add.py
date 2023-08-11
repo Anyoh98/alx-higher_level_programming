5c1bfe034776142afb2aba533acab19ca3d1e046
Anyoh98 <fonachuanyoh@yahoo.com>
2023-08-11 00:33:11 -0700

    just testing

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,8 +4,9 @@ if __name__ == "__main__":
     arg_index_count = len(sys.argv) - 1
     if arg_index_count == 0:
         print("0")
-    arguments = sys.argv[1:]
-    total_sum = 0
-    for arg in arguments:
-        total_sum += int(arg)
-    print("{}".format(total_sum))
+    else:
+        arguments = sys.argv[1:]
+        total_sum = 0
+        for arg in arguments:
+            total_sum += int(arg)
+        print("{}".format(total_sum))
