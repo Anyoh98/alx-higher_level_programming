4e7be8565be6c4db31ca975abf80d6a584496559
Anyoh98 <fonachuanyoh@yahoo.com>
2023-08-16 16:06:07 -0700

    just testing

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
     best_key = None
     best_value = float('-inf')  # Start with negative infinity as a baseline
 
+    if not a_dictionary:
+        return (None)
     for key, value in a_dictionary.items():
         if value > best_value:
             best_key = key
