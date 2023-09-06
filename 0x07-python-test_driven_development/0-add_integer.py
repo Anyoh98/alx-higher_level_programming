087321b2cfc1fe9c1d1651797c0c916cc7fa0fff
Anyoh98 <fonachuanyoh@yahoo.com>
2023-09-06 13:37:27 -0700

    fixed the he pycodestyle errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,13 +6,12 @@ def add_integer(a, b=98):
     Raises:
         TypeError: if 'a' or 'b' are not floats or integers
     """
-    if not isinstance(a, (int,float)):
-        raise TypeError ("a must be an integer")
-    if not isinstance (b, (int, float)):
-        raise TypeError ("b must be an integer")
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
     return a + b
-
