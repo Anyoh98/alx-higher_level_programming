86b4ff58f938ba29b01100ea0f463e990cec14a3
Anyoh98 <fonachuanyoh@yahoo.com>
2023-09-13 18:48:58 -0700

    tesing

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pyhton3
+""" defines a class checking inherited function """
+
+
+class BaseGeometry:
+    """
+    This is the BaseGeometry class.
+
+    Methods:
+        area(self): Raises an Exception with
+        the message "area() is not implemented."
+
+    Attributes:
+        None
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an
+        Exception with the message "area() is not implemented."
+        """
+        raise Exception("area() is not implemented")
