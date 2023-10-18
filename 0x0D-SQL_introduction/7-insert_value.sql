0830a9d5e555f31ca42e11b3a98d9d85a9edd91d
Anyoh98 <fonachuanyoh@yahoo.com>
2023-10-18 14:11:54 +0100

    testing

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Script that inserst a new row into table in db in MYMYSQL server
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best school");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
